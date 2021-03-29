d18a9a97a921be2561d3896eb68f49389f54c7fe
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-03-29 18:05:38 +0200

    add bankaccount and client classes

---
+++ console-bank-c/console-bank-c/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Client
+{
+    public int Id
+    {
+        get { return Id; }
+        set { Id = value; }
+    }
+    public string Name
+    {
+        get { return Name; }
+        set { Name = value; }
+    }
+    public int DateJoined
+    {
+        get { return DateJoined; }
+        set { DateJoined = value; }
+    }
+    public Client(int Id, string Name, int DateJoined)
+    {
+        this.Id = Id;
+        this.Name = Name;
+        this.DateJoined = DateJoined;
+    }
+
+    
+}
---
+++ console-bank-c/console-bank-c/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class BankAccount
+    {
+        public string Client
+        {
+            get { return Client; }
+            set { Client = value; }
+        }
+        public double Balance
+        {
+            get { return Balance; }
+            set { Balance = value; }
+        }
+        public string Type
+        {
+            get { return Type; }
+            set { Type = value; }
+        }
+        public BankAccount(string Client, double Balance, string type)
+        {
+            this.Client = Client;
+            this.Balance = Balance;
+            this.Type = Type;
+        }
+
+        public void CheckBalance()
+        {
+            //don't know yet what to do
+        }
+
+        public void Withdraw()
+        {
+            //
+        }
+
+        public void Deposit() 
+        { 
+            //
+        }
+    }
+
+}
